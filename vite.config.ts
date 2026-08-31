4e161c8b2005a33a7b19d2568e09250810c11693
Giotina-Hung <222237403+Giotina-Hung@users.noreply.github.com>
2026-08-31 21:18:23 +0800

    fix: set base URL to /htt_mouses/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/htt_mouses/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
